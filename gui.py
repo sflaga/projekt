cbb2b67fff96f22b6d4970d91e26d42583b99335
sflaga <92295581+sflaga@users.noreply.github.com>
2023-06-15 22:04:01 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,51 +1,96 @@
 import tkinter as tk
-from tkinter import messagebox
+import sqlite3
 
-class GUI:
-    def __init__(self, database):
-        self.database = database
-        self.window = tk.Tk()
-        self.window.title("Aplikacja bazodanowa")
-
-        self.label = tk.Label(self.window, text="Witaj w aplikacji bazodanowej!")
-        self.label.pack()
-
-        self.entry = tk.Entry(self.window)
-        self.entry.pack()
-
-        self.button = tk.Button(self.window, text="Dodaj rekord", command=self.add_record)
-        self.button.pack()
-
-    def add_record(self):
-        record = self.entry.get()
-        self.database.insert_record(record)
-        messagebox.showinfo("Sukces", "Rekord został dodany do bazy danych!")
-
-    def run(self):
-        self.window.mainloop()
+# Połączenie z bazą danych
+conn = sqlite3.connect("baza_danych.db")
+cursor = conn.cursor()
 
 
-class Database:
-    def __init__(self):
-        self.connection = sqlite3.connect("database.db")
-        self.cursor = self.connection.cursor()
-        self.create_tables()
+class SklepRowerowy:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Sklep rowerowy")
+        self.master.geometry("400x300")
 
-    def create_tables(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER)")
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS Products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price REAL)")
+        # Ustawienie koloru tła
+        self.master.configure(bg="lightgray")
 
-    def insert_record(self, record):
-        table_name, values = record
-        query = f"INSERT INTO {table_name} VALUES (NULL, {values})"
-        self.cursor.execute(query)
-        self.connection.commit()
+        # Przyciski do wyświetlania danych
+        ramy_button = tk.Button(
+            self.master,
+            text="Wyświetl Ramy",
+            command=self.wyswietl_ramy,
+            bg="blue",
+            fg="white",
+        )
+        ramy_button.pack(pady=10)
 
-    def close(self):
-        self.connection.close()
+        kola_button = tk.Button(
+            self.master,
+            text="Wyświetl Koła",
+            command=self.wyswietl_kola,
+            bg="green",
+            fg="white",
+        )
+        kola_button.pack(pady=10)
 
+        akcesoria_button = tk.Button(
+            self.master,
+            text="Wyświetl Akcesoria",
+            command=self.wyswietl_akcesoria,
+            bg="orange",
+            fg="white",
+        )
+        akcesoria_button.pack(pady=10)
 
-if __name__ == "__main__":
-    database = Database()
-    gui = GUI(database)
-    gui.run()
+        czesci_button = tk.Button(
+            self.master,
+            text="Wyświetl Części",
+            command=self.wyswietl_czesci,
+            bg="purple",
+            fg="white",
+        )
+        czesci_button.pack(pady=10)
+
+        # Przycisk do usuwania pozycji
+        usun_button = tk.Button(
+            self.master,
+            text="Usuń pozycję",
+            command=self.usun_pozycje,
+            bg="red",
+            fg="white",
+        )
+        usun_button.pack(pady=10)
+
+    def wyswietl_ramy(self):
+        # Utworzenie nowego okna dla kategorii "Ramy"
+        okno_ramy = tk.Toplevel(self.master)
+        okno_ramy.title("Ramy")
+        okno_ramy.geometry("400x300")
+
+        # Ustawienie koloru tła
+        okno_ramy.configure(bg="lightblue")
+
+        # Pobranie danych z tabeli "ramy"
+        cursor.execute("SELECT * FROM ramy")
+        dane = cursor.fetchall()
+
+        # Wyświetlenie danych w listboxie
+        listbox = tk.Listbox(okno_ramy)
+        listbox.pack(side="left", fill="both", expand=True)
+
+        # Dodanie pasku przewijania dla listboxa
+        scrollbar = tk.Scrollbar(okno_ramy)
+        scrollbar.pack(side="right", fill="y")
+
+        # Przypisanie paska przewijania do listboxa
+        listbox.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=listbox.yview)
+
+        # Wyświetlenie danych w listboxie
+        for rama in dane:
+            listbox.insert("end", rama)
+
+    def wyswietl_kola(self):
+        # Utworzenie nowego okna dla kategorii "Koła"
+        okno_kola =
