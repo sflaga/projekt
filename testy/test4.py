b3a2951fad9f579886dcf23d6b6aae16e43d73ec
sflaga <92295581+sflaga@users.noreply.github.com>
2023-06-20 16:23:03 +0200

    Update test4.py

--- testy/test4.py
+++ testy/test4.py
@@ -1,1 +1,14 @@
+#Test edytowania rekordow
+import sqlite3
 
+# Utworzenie połączenia z bazą danych
+conn = sqlite3.connect('baza_danych.db')
+c = conn.cursor()
+
+# Edycja rekordu w tabela1
+c.execute("UPDATE tabela1 SET nazwa='Produkt X', cena=30.0 WHERE id=1")
+conn.commit()
+
+# Sprawdzenie czy rekord został zmieniony
+c.execute("SELECT * FROM tabela1 WHERE id=1 AND nazwa='Produkt X' AND cena=30.0")
+record_exists = c.fetchone() is not None
